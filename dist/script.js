ba554128f7570a003d11c3f8bd89c0472dc5ff1e
redundans <jesper@klandestino.se>
2021-05-17 08:51:15 +0200

    Add mobile menu.

--- dist/script.js
+++ dist/script.js
@@ -9,5 +9,22 @@ document.addEventListener(
 			nav_toggle.classList.toggle('bg-white');
 			nav_toggle.classList.toggle('text-black');
 		}, false);
+
+
+		let nav_toggle_mob = document.querySelector('#nav_toggle_mob');
+		let nav_mob = document.querySelector('#nav_mob');
+		nav_toggle_mob.addEventListener('click', function() {
+			nav_mob.classList.toggle('hidden');
+			nav_toggle_mob.classList.toggle('open');
+		}, false);
+
+
+		let nav_first_toggle_mob = document.querySelector('#nav_first_toggle_mob');
+		let nav_first_mob = document.querySelector('#nav_first_mob');
+		nav_first_toggle_mob.addEventListener('click', function() {
+			nav_first_toggle_mob.classList.toggle('bg-white');
+			nav_first_toggle_mob.classList.toggle('text-black');
+			nav_first_mob.classList.toggle('hidden');
+		}, false);
 	}
 );
